93e74e110bbec250db95b0e585d8751a69d54757
Jeremy Plott <jeremyplott@gmail.com>
2019-08-26 19:39:20 -0400

    initial commit

---
+++ HelloWorldProjectSolution/HelloWorldProject/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+//here's a comment
+
+namespace HelloWorldProject {
+
+    class Program {
+
+        static void Main(string[] args) {
+            Console.Write("Hello C# Bootcamp World! ");
+            Console.WriteLine("At MAX Technical Training.");
+            Console.WriteLine("Write vs WriteLine can be seen here.");
+
+            int daysInBootcamp = 6;
+            if (daysInBootcamp == 6) {
+                Console.WriteLine("Yes.");
+            } else {
+                Console.WriteLine("No.");
+            }
+
+            string instructor = "";
+            string lastname = "Doud";
+            instructor = "Greg";
+
+            Console.Write("The instructor is named ");
+            Console.WriteLine(instructor);
+
+            Console.WriteLine(instructor + " " + lastname);
+            Console.WriteLine("The instructor is named " + instructor + " " + lastname);
+        }
+    }
+}
---
+++ PrintingHW/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PrintingHW {
+
+    class Program {
+
+        static void Main(string[] args) {
+
+            //Define variables
+            string firstname = "Jeremy";
+            string lastname = "Plott";
+            string city = "City";
+            string state = "??";
+            string zip = "12345";
+            string address = "1234 Hello World St.";
+            string phone = "123-456-7890";
+            string email = "notenoughlette@aol.com";
+
+
+            Console.WriteLine("Hello World! Here's all of my personal information!");
+            Console.WriteLine("My name is " + firstname + " " + lastname + ".");
+            Console.WriteLine("I live at " + address + ", " + city + " " + state + ", " + zip + "."); //all of these spaces could be put into the variables for cleaner code
+            Console.Write("My phone number is " + phone + " and ");
+            Console.Write("my email address is " + email);
+            Console.WriteLine(". Tune in next week for my social!");
+        }
+    }
+}
