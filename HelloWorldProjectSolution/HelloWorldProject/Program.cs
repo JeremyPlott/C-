4e8776e9bdff97408070eddc799e1ddd93c7af1a
Jeremy <JeremyPlott@gmail.com>
2019-08-27 11:50:43 -0400

    Added while / for loops

--- HelloWorldProjectSolution/HelloWorldProject/Program.cs
+++ HelloWorldProjectSolution/HelloWorldProject/Program.cs
@@ -27,6 +27,52 @@ namespace HelloWorldProject {
 
             Console.WriteLine(instructor + " " + lastname);
             Console.WriteLine("The instructor is named " + instructor + " " + lastname);
+
+            //looping with while
+
+            var ints = new int[] { 1, 2, 3, 4, 5 }; //don't forget about zero base
+            var total = 0;
+            var index = 0;
+
+            while (index < 5) {
+                total = total + (ints[index] * ints[index]);
+                index = index + 1;
+            }
+            Console.WriteLine("The total is " + total);
+
+
+            //looping with for
+
+            //first part happens once to create increment variable. Middle is boolean which then executes body code. 
+            //Last executes after body, then returns to middle.
+            total = 0;
+            for (var idx = 0; idx < 5; idx = idx +1) {
+                total = total + (ints[idx] * ints[idx]);
+            }
+            Console.WriteLine("The total is " + total);
+
+            //compute the average exercise
+
+            var scores = new int[] { 738, 609, 307, 959, 805, 689, 402, 972, 359, 140 };
+
+            total = 0;
+            for(var entry = 0; entry < 10; entry = entry + 1) {
+                total = total + scores[entry];
+            }
+            var avgscore = total / 10;
+
+            Console.WriteLine("The average score is " + avgscore);
+
+
+            //cleaner code
+            total = 0;
+            avgscore = 0;
+            for (var entry = 0; entry < scores.Length; entry = entry + 1) {
+                total = total + scores[entry];
+            }
+            avgscore = total / scores.Length;
+
+            Console.WriteLine("The average score is " + avgscore);
         }
     }
 }
